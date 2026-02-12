b845d35da40ba7b8293570470f8bebb23a8147c5
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2026-02-12 16:13:58 -0300

    feat: v1.0.0 - MVP com API, frontend e gráfico de alocação

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,45 @@
 const API_URL = "http://localhost:3000";
 
+let grafico; //variavel global
+
+async function carregarGrafico() {
+    const response = await fetch(`${API_URL}/ativos`);
+    const ativos = await response.json();
+
+    let labels = [];
+    let valores = [];
+
+    for (let ativo of ativos){
+        const resumoResponse = await fetch(`${API_URL}/resumo/${ativo.id}`);
+        const resumo = await resumoResponse.json();
+
+        if (resumo.valor_atual > 0) {
+            labels.push(resumo.nome);
+            valores.push(resumo.valor_atual);
+        }
+    }
+
+    const ctx = document.getElementById("graficoAlocacao").getContext("2d");
+
+    if (grafico) {
+        grafico.destroy();
+    }
+
+    grafico = new Chart(ctx, {
+        type: "pie",
+        data: {
+            labels: labels,
+            datasets: [{
+                data: valores
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+}
+
 async function adicionarAtivo() {
     const nome = document.getElementById("nome").value;
     const tipo = document.getElementById("tipo").value;
@@ -38,6 +78,35 @@ async function adicionarAporte() {
     carregarAtivos();
 }
 
+async function carregarCarteira() {
+    const response = await fetch(`${API_URL}/carteira`);
+    const data = await response.json();
+    document.getElementById("valor-carteira").innerHTML =
+    `R$ ${data.valor_total.toFixed(2)}`;
+}
+
+async function atualizarPreco(id) {
+    const novoPreco = document.getElementById(`preco-${id}`).value;
+    await fetch(`${API_URL}/ativos/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            preco_atual: Number(novoPreco)
+        })
+    });
+
+    carregarAtivos();
+}
+
+async function deletarAtivo(id){
+    await fetch(`${API_URL}/ativos/${id}`, {
+        method: "DELETE"
+    });
+
+    carregarAtivos();
+    //Como configurou ON DELETE CASCADE, os aportes são apagados automaticamente
+}
+
 async function carregarAtivos() {
     const response = await fetch(`${API_URL}/ativos`);
     const ativos = await response.json();
@@ -49,6 +118,13 @@ async function carregarAtivos() {
         const resumoResponse = await fetch(`${API_URL}/resumo/${ativo.id}`);
         const resumo = await resumoResponse.json();
 
+        const carteiraResponse = await fetch(`${API_URL}/carteira`);
+        const carteira = await carteiraResponse.json();
+        const percentual = 
+            carteira.valor_total > 0
+            ? (resumo.valor_atual / carteira.valor_total) * 100
+            : 0;
+
         const div = document.createElement("div");
         div.innerHTML = `
             <hr>
@@ -56,10 +132,20 @@ async function carregarAtivos() {
             Quantidade: ${resumo.quantidade_total}<br>
             Investido: R$ ${resumo.total_investido}<br>
             Valor Atual: R$ ${resumo.valor_atual}<br>
-            Lucro/Prejuízo: R$ ${resumo.lucro_prejuizo}
+            Percentual da Carteira: ${percentual.toFixed(2)}%<br>
+            Lucro/Prejuízo: R$ ${resumo.lucro_prejuizo}<br><br>
+
+            Novo preço:
+            <input type="number" id="preco-${ativo.id}" placeholder="Novo preço">
+            <button onclick="atualizarPreco(${ativo.id})">Atualizar</button>
+
+            <button onclick="deletarAtivo(${ativo.id})">Deletar Ativo</button>
         `;
         container.appendChild(div);
     }
+    
+    carregarCarteira();
+    carregarGrafico();
 }
 
 carregarAtivos();
